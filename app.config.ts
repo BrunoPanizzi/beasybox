d54a3800701ea28e0cb32abc4fea93a417e6692f
Bruno Panizzi <brunopanizziq@gmail.com>
2025-05-29 17:51:43 -0300

    fix build options

--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config"
+import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  server: {
+    preset: "bun",
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+  },
   tsr: {
-    appDirectory: 'src',
+    appDirectory: "src",
   },
   vite: {
     plugins: [
       tsConfigPaths({
-        projects: ['./tsconfig.json'],
+        projects: ["./tsconfig.json"],
       }),
     ],
   },
